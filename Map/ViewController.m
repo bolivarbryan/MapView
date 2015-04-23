3fc0f42dc73750a585879acc37389be6a4e7414d
Bryan A Bolivar M <boli@Mac-mini-de-Bryan.local>
2015-04-23 17:26:13 -0500

    first commit

--- Map/ViewController.m
+++ Map/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
-@interface ViewController ()
+@interface ViewController (){
+    CLLocationManager *locationManager;
+
+}
 
 @end
 
@@ -16,7 +21,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.mapa.showsUserLocation = YES;
+    self.mapa.mapType = MKMapTypeHybrid;
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if(IS_OS_8_OR_LATER) {
+        [locationManager requestAlwaysAuthorization];
+    }
+    
+    [locationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +39,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)mapView:(MKMapView *)aMapView didUpdateUserLocation:(MKUserLocation *)aUserLocation {
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.005;
+    span.longitudeDelta = 0.005;
+    CLLocationCoordinate2D location;
+    location.latitude = aUserLocation.coordinate.latitude;
+    location.longitude = aUserLocation.coordinate.longitude;
+    region.span = span;
+    region.center = location;
+    [aMapView setRegion:region animated:YES];
+}
 @end
