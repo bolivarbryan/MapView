bbe60b25a1b4a0ae3e45dd55c2070772adf935b1
Bryan A Bolivar M <boli@Mac-mini-de-Bryan.local>
2015-04-23 17:08:37 -0500

    Initial Commit

---
+++ Map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map
+//
+//  Created by Bryan A Bolivar M on 4/23/15.
+//  Copyright (c) 2015 Bolivar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map
+//
+//  Created by Bryan A Bolivar M on 4/23/15.
+//  Copyright (c) 2015 Bolivar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
